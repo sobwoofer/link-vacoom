30dba61486820e0886eb5e16fda5fd95a5f35123
Volynets Ivan <volynets.ivan@pdffiller.team>
2020-02-17 14:43:33 +0200

    fixed

--- linkVacuum/items.py
+++ linkVacuum/items.py
@@ -8,7 +8,7 @@
 import scrapy
 
 
-class LikvacuumItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class LinkvacuumItem(scrapy.Item):
+    title = scrapy.Field()
+    body = scrapy.Field()
+    date = scrapy.Field()
